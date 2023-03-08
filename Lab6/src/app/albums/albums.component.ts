e0a6fbfd148ff58338a77efbca98dda07cc8fa15
21B031249VK <98973980+21B031249VK@users.noreply.github.com>
2023-03-08 19:41:31 +0600

    Add files via upload

---
+++ Lab6/src/app/albums/albums.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import {Album} from "../models";
+import {ALBUMS} from "../fake-db";
+import {AlbumService} from "../album.service";
+
+@Component({
+  selector: 'app-albums',
+  templateUrl: './albums.component.html',
+  styleUrls: ['./albums.component.css']
+})
+export class AlbumsComponent implements OnInit {
+  albums: Album[];
+  loaded: boolean;
+  constructor(private albumService: AlbumService) {
+    this.albums = [];
+    this.loaded = true;
+  }
+
+  ngOnInit(): void {
+   // this.albums = ALBUMS;
+    this.getAlbums();
+    }
+
+  getAlbums(){
+    this.loaded = false;
+    this.albumService.getAlbums().subscribe((albums) =>{
+      this.albums = albums;
+      this.loaded = true;
+    })
+  }
+  /*addAlbum(){
+    this.loaded = false;
+    this.albumService.addAlbum(this.newAlbum).subscribe((album) => {
+      // this.posts.push(post);
+      this.albums.unshift(album);
+      this.loaded = true;
+      this.newAlbum = {} as Album;
+    });
+  }
+*/
+
+}
